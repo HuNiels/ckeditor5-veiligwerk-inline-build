da7e4acec1c9dbbb7cf404f558b9c74b250cb7e1
Niels <niels@veiligwerk.net>
2024-12-17 09:47:33 +0100

    only essentials

--- build/ckeditor.d.ts
+++ build/ckeditor.d.ts
@@ -22,13 +22,13 @@ import { PageBreak } from '@ckeditor/ckeditor5-page-break';
 import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
 import { RemoveFormat } from '@ckeditor/ckeditor5-remove-format';
 import { SpecialCharacters } from '@ckeditor/ckeditor5-special-characters';
-import { SpecialCharactersCurrency, SpecialCharactersMathematical, SpecialCharactersEssentials } from '@ckeditor/ckeditor5-special-characters';
+import { SpecialCharactersEssentials } from '@ckeditor/ckeditor5-special-characters';
 import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
 import { TextTransformation } from '@ckeditor/ckeditor5-typing';
 import { FileRepository } from "@ckeditor/ckeditor5-upload";
 import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
 declare class InlineEditor extends InlineEditorBase {
-    static builtinPlugins: (typeof Alignment | typeof Autoformat | typeof BlockQuote | typeof Bold | typeof Base64UploadAdapter | typeof CloudServices | typeof Essentials | typeof FontFamily | typeof FontSize | typeof Heading | typeof Image | typeof ImageCaption | typeof ImageStyle | typeof ImageToolbar | typeof ImageUpload | typeof Indent | typeof Italic | typeof Link | typeof List | typeof MediaEmbed | typeof PageBreak | typeof Paragraph | typeof PictureEditing | typeof RemoveFormat | typeof SpecialCharacters | typeof SpecialCharactersEssentials | typeof SpecialCharactersCurrency | typeof SpecialCharactersMathematical | typeof Strikethrough | typeof Table | typeof TableToolbar | typeof TextTransformation | typeof Underline | typeof FileRepository | typeof HorizontalLine | typeof FontColor | typeof FontBackgroundColor | typeof Subscript | typeof Superscript)[];
+    static builtinPlugins: (typeof Alignment | typeof Autoformat | typeof BlockQuote | typeof Bold | typeof Base64UploadAdapter | typeof CloudServices | typeof Essentials | typeof FontFamily | typeof FontSize | typeof Heading | typeof Image | typeof ImageCaption | typeof ImageStyle | typeof ImageToolbar | typeof ImageUpload | typeof Indent | typeof Italic | typeof Link | typeof List | typeof MediaEmbed | typeof PageBreak | typeof Paragraph | typeof PictureEditing | typeof RemoveFormat | typeof SpecialCharacters | typeof SpecialCharactersEssentials | typeof Strikethrough | typeof Table | typeof TableToolbar | typeof TextTransformation | typeof Underline | typeof FileRepository | typeof HorizontalLine | typeof FontColor | typeof FontBackgroundColor | typeof Subscript | typeof Superscript)[];
     static defaultConfig: {
         toolbar: (string | {
             label: string;
@@ -52,7 +52,7 @@ declare class InlineEditor extends InlineEditorBase {
     };
 }
 declare class ClassicEditor extends ClassicEditorBase {
-    static builtinPlugins: (typeof Alignment | typeof Autoformat | typeof BlockQuote | typeof Bold | typeof Base64UploadAdapter | typeof CloudServices | typeof Essentials | typeof FontFamily | typeof FontSize | typeof Heading | typeof Image | typeof ImageCaption | typeof ImageStyle | typeof ImageToolbar | typeof ImageUpload | typeof Indent | typeof Italic | typeof Link | typeof List | typeof MediaEmbed | typeof PageBreak | typeof Paragraph | typeof PictureEditing | typeof RemoveFormat | typeof SpecialCharacters | typeof SpecialCharactersEssentials | typeof SpecialCharactersCurrency | typeof SpecialCharactersMathematical | typeof Strikethrough | typeof Table | typeof TableToolbar | typeof TextTransformation | typeof Underline | typeof FileRepository | typeof HorizontalLine | typeof FontColor | typeof FontBackgroundColor | typeof Subscript | typeof Superscript)[];
+    static builtinPlugins: (typeof Alignment | typeof Autoformat | typeof BlockQuote | typeof Bold | typeof Base64UploadAdapter | typeof CloudServices | typeof Essentials | typeof FontFamily | typeof FontSize | typeof Heading | typeof Image | typeof ImageCaption | typeof ImageStyle | typeof ImageToolbar | typeof ImageUpload | typeof Indent | typeof Italic | typeof Link | typeof List | typeof MediaEmbed | typeof PageBreak | typeof Paragraph | typeof PictureEditing | typeof RemoveFormat | typeof SpecialCharacters | typeof SpecialCharactersEssentials | typeof Strikethrough | typeof Table | typeof TableToolbar | typeof TextTransformation | typeof Underline | typeof FileRepository | typeof HorizontalLine | typeof FontColor | typeof FontBackgroundColor | typeof Subscript | typeof Superscript)[];
     static defaultConfig: {
         toolbar: (string | {
             label: string;
